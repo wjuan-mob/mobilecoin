679e81366ddf78e581532452171922037e985b84
the-real-adammork <adammork@gmail.com>
2021-11-18 10:57:33 -0800

    correct arg

--- tools/enclaver.sh
+++ tools/enclaver.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export NETWORK=prod.mobilecoin.com; ./download_sigstruct.sh 
+export NETWORK=$1; ./download_sigstruct.sh 
 
 echo -e "\nConsensus Enclave"
 cargo run -p mc-sgx-css-dump --release 2>/dev/null < ./consensus-enclave.css | grep MRENCLAVE | sed 's/,//'
